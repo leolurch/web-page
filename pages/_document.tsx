567f484c09d9687f72776f81bcc68001fa0895ab
leolurch <40560457+leolurch@users.noreply.github.com>
2023-08-15 00:55:14 +0200

    fix build and add favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,7 +3,9 @@ import { Html, Head, Main, NextScript } from 'next/document';
 export default function Document() {
   return (
     <Html lang="en">
-      <Head></Head>
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body
         style={{
           backgroundColor: 'rgb(214 211 209)',
